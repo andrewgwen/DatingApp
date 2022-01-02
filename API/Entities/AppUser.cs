20f3f2d422d5147aeab713fce710d5a62226eda6
AMAN GUPTA <er.amangupta1604@gmail.com>
2022-01-02 21:12:18 +0530

    end of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,5 +9,8 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserNAme { get; set; }
+        public byte[] passwordHash { get; set; }
+        public byte[] passwordSalt { get; set; }
+
     }
 }
